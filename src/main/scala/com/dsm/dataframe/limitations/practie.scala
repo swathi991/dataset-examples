cf7f90eb029e0c4ce951ab240a7ddc1672a9bcb4
swathi991 <swathis031@gmail.com>
2021-02-09 12:09:09 +0530

    Initial commit

---
+++ src/main/scala/com/dsm/dataframe/limitations/practie.scala
@@ -0,0 +1,60 @@
+package com.dsm.dataframe.limitations
+
+import com.dsm.utils.Constants
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.types._
+
+object practie{
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder
+      .master("local[*]")
+      .appName("Dataframe Example")
+      .getOrCreate()
+    spark.sparkContext.setLogLevel(Constants.ERROR)
+
+    spark.sparkContext.hadoopConfiguration.set("fs.s3n.awsAccessKeyId", Constants.ACCESS_KEY)
+    spark.sparkContext.hadoopConfiguration.set("fs.s3n.awsSecretAccessKey", Constants.SECRET_ACCESS_KEY)
+
+    println("\nCreating dataframe from CSV file using 'SparkSession.read.format()',")
+    val finSchema = new StructType()
+      .add("id", IntegerType,true)
+      .add("has_debt", BooleanType,true)
+      .add("has_financial_dependents", BooleanType,true)
+      .add("has_student_loans", BooleanType,true)
+      .add("income", DoubleType,true)
+
+    val finDf = spark.read
+      .option("header", "false")
+      .option("delimiter", ",")
+      .format("csv")
+      .schema(finSchema)
+      .load("s3n://" + Constants.S3_BUCKET + "/finances.csv")
+
+    finDf.printSchema()
+    finDf.show()
+
+    println("Creating dataframe from CSV file using 'SparkSession.read.csv()',")
+
+    var financeDf = spark.read
+      .option("mode", "DROPMALFORMED")
+      .option("header", "false")
+      .option("delimiter", ",")
+      .option("inferSchema", "true")
+      .csv("s3n://" + Constants.S3_BUCKET + s"/finances.csv")
+      .toDF("id", "has_debt", "has_financial_dependents", "has_student_loans", "income")
+
+    println("# of partitions = " + finDf.rdd.getNumPartitions)
+    financeDf.printSchema()
+    financeDf.show()
+
+    financeDf
+      .repartition(2)
+      .write
+      .mode(SaveMode.Append)
+      .option("header", "true")
+      .option("delimiter", "~")
+      .csv("s3n://" + Constants.S3_BUCKET + "/fin")
+
+    spark.close()
+  }
+}
---
+++ src/main/scala/com/dsm/intro/to/dataset/OperationOnDomainObject.scala
@@ -0,0 +1,37 @@
+package com.dsm.intro.to.dataset
+
+import com.dsm.model.Person
+import com.dsm.utils.Constants
+import org.apache.spark.sql.SparkSession
+
+object OperationOnDomainObject {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder()
+      .master("local[*]")
+      .appName("Comparision with Dataframe")
+      .getOrCreate()
+    spark.sparkContext.setLogLevel(Constants.ERROR)
+
+    val persons = Seq(
+      Person("Sidharth", 30),
+      Person("Vinay", 26),
+      Person("Piyush", 25)
+    )
+
+    // Create RDD of Person
+    val personRdd = spark.sparkContext.makeRDD(persons)
+    personRdd.collect().foreach(println)
+    println(personRdd.collect())
+
+    // Create Dataset from an RDD[Person]
+    import spark.implicits._
+    val personDs = spark.createDataset(personRdd)
+    personDs.show()
+
+    // We get back RDD[Person]
+    println(personDs.rdd.collect())
+
+    spark.stop()
+  }
+}
